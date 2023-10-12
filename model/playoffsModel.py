fced72f839082b0887a2b325a187662d0d622468
Ricardo Inácio <ricardo.inacio.2002@gmail.com>
2023-10-12 16:19:55 +0100

    made plots and changed model

--- model/playoffsModel.py
+++ model/playoffsModel.py
@@ -5,22 +5,36 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 def predictPlayoffs(db, table, target, split_year):
-    database = "../db/" + db + ".db"
-    conn = sqlite3.connect()
+    database = "../" + db + ".db"
+    conn = sqlite3.connect(database)
     query = "SELECT * FROM " + table + ";"
     data = pd.read_sql_query(query, conn)
 
     data["year"] = pd.to_numeric(data["year"])
+    data = data.drop(
+        [
+            "lgID",
+            "franchID",
+            "tmID",
+            "confID",
+            "name",
+            "firstRound",
+            "arena",
+            "semis",
+            "finals",
+        ],
+        axis=1,
+    )
 
     # manually spearating the data so it doesn't split mid-year
     train_data = data[data["year"] < split_year]
     test_data = data[data["year"] >= split_year]
 
-    X_train = train_data.drop(["playoffs"], axis=1)
-    y_train = train_data["playoffs"]
+    X_train = train_data.drop(["playoff"], axis=1)
+    y_train = train_data["playoff"]
 
-    X_test = test_data.drop(["playoffs"], axis=1)
-    y_test = test_data["playoffs"]
+    X_test = test_data.drop(["playoff"], axis=1)
+    y_test = test_data["playoff"]
 
     # escolher o modelo a utilizar
     model = DecisionTreeClassifier()
@@ -31,3 +45,6 @@ def predictPlayoffs(db, table, target, split_year):
     print(predictions)
 
     conn.close()
+
+
+predictPlayoffs("database", "Teams", "playoff", 4)
